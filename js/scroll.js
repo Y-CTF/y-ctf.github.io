52e171e047f0d7f192615ad91cdc6bbc8e84420b
David Pellissier <dav.pellissier@gmail.com>
2022-05-11 12:49:47 +0200

    fix scroll I hope

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,10 @@
 // only on mobile (src: https://www.geeksforgeeks.org/how-to-detect-a-mobile-device-in-jquery/)
-if (window.matchMedia("(max-width: 767px)").matches) {
-    
+if (window.matchMedia("(max-width: 1080px)").matches) {
     // Source: https://codemyui.com/hide-header-navigation-on-scroll-down-and-show-on-scroll-up/
     $(document).ready(function () {
-
         'use strict';
-
-        var c, currentScrollTop = 0,
-            navbar = $('nav');
+        var c, currentScrollTop = 0;
+        var navbar = $('nav');
 
         $(window).scroll(function () {
             var a = $(window).scrollTop();
@@ -21,9 +18,7 @@ if (window.matchMedia("(max-width: 767px)").matches) {
                 navbar.removeClass("scrollUp");
             }
             c = currentScrollTop;
-
         });
-
     });
 }
 
